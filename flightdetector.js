9e4586e7c659e26aa733f6ebe98eedf9f1039041
Rob <stenton.toledo@gmail.com>
2013-01-27 22:02:40 -0800

    Update flightdetector.js

--- flightdetector.js
+++ flightdetector.js
@@ -1,17 +1,17 @@
 
 var dailyFlights = [];
 function pushFlight() {
-	for(i=0; i < 3; i++) {
+	for(i=0; i<3; i++) {
 		function flightDetails(airline, time, destination){
 			this.airline = airline;
 			this.time = time;
-			this.destination = destination;s
+			this.destination = destination;
 		}
 		var airlinePrompt = prompt("Enter airline");
 		var timePrompt = prompt("Enter departure time");
 		var arrivalCityPrompt = prompt("Enter arrival city");
 		dailyFlights.push(new flightDetails(airlinePrompt, timePrompt, arrivalCityPrompt));
+		console.log(dailyFlights[i]);
 	}
 }
 pushFlight();
-console.log(dailyFlights);
